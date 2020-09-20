6242920a06f25e70165196e9e806b102af4df52d
VanshikaaGupta <70034183+VanshikaaGupta@users.noreply.github.com>
2020-09-20 12:44:56 +0530

    Add files via upload

---
+++ precache-manifest.f97ae5102259b78d584f6dfb48369b8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11136df9f3d0edb7774f13cc63b6dc2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4391616ce3f69f5845f7",
+    "url": "/static/js/2.43b9fa22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.43b9fa22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dea7e4a9e481e0256c83",
+    "url": "/static/js/main.274a4f1d.chunk.js"
+  },
+  {
+    "revision": "ffe148ba9c987666fc58",
+    "url": "/static/js/runtime-main.451dc41f.js"
+  }
+]);
